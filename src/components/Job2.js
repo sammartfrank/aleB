6e59ed1b8dfb37d84d57e4350229d7826a78005f
sammartfrank <franksammartino7@gmail.com>
2019-01-30 14:37:51 -0300

    content

--- src/components/Job2.js
+++ src/components/Job2.js
@@ -3,9 +3,9 @@ import Vermas from './Vermas.js';
 
 var results = [
 	{title:'food netwoork', subtitle: 'Campaña para el gobierno de la ciudad', imagenes: [], videos: []},
-	{title:'trabajo 2', subtitle: 'Campaña del desierto', imagenes: [], videos: []},
-	{title:'trabajo 3', subtitle: 'Campaña del oceano', imagenes: [], videos: []},
-	{title:'trabajo 4', subtitle: 'Campaña del culo', imagenes: [], videos: []},
+	{title:'trabajo 2', subtitle: 'campaña del desierto', imagenes: [], videos: []},
+	{title:'trabajo 3', subtitle: 'campaña del oceano', imagenes: [], videos: []},
+	{title:'trabajo 4', subtitle: 'campaña del culo', imagenes: [], videos: []},
 	];
 
 
