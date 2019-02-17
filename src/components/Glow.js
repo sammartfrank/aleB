3801d70d5fe12105d000cf9635755552c1e5a5bb
Ale Barredo <ale.barredo@gmail.com>
2019-02-17 03:16:08 -0300

    Pajadings

--- src/components/Glow.js
+++ src/components/Glow.js
@@ -15,7 +15,7 @@ class Glow extends Component {
 							from={{ opacity:0, transform: 'translateY(0)',zIndex:'1045'  }}
 							to={{ opacity:1, transform: 'translateY(-150px)', zIndex:'1'}}>
 								{props => <div style={props} className="preft">
-								<h1>tv&print</h1>
+								<h1>tV&pRint</h1>
 								</div>}
 					</Spring>
 				</div>
